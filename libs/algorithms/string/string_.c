00327c75e7764e9e77b2ac1e2003992952ba3213
spynotspy <jackson403@yandex.ru>
2022-03-08 20:50:18 +0300

    added specs

--- libs/algorithms/string/string_.c
+++ libs/algorithms/string/string_.c
@@ -3,6 +3,7 @@
 //
 #include "string_.h"
 
+//№1 возвращает количество символов в строке (не считая ноль-символ)
 size_t strlen_(const char *begin) {
     char *end = begin;
     while (*end != '\0')
@@ -11,6 +12,9 @@ size_t strlen_(const char *begin) {
     return end - begin;
 }
 
+//№2 возвращает указатель
+//на первый элемент с кодом ch, расположенным на ленте памяти между
+//адресами begin и end не включая end. Если символ не найден, возвращается значение end.
 char *find(char *begin, char *end, int ch) {
     while (begin != end && *begin != ch)
         begin++;
@@ -18,6 +22,9 @@ char *find(char *begin, char *end, int ch) {
     return begin;
 }
 
+//№3 возвращает указатель на первый
+//символ, отличный от пробельных1,
+// расположенный на ленте памяти, начиная с begin и заканчивая ноль-символом. Если символ не найден, возвращается адрес первого ноль-символа
 char *findNonSpace(char *begin) {
     while (*begin != '\0' && isspace(*begin))
         begin++;
@@ -25,6 +32,8 @@ char *findNonSpace(char *begin) {
     return begin;
 }
 
+//№4 возвращает указатель на первый пробельный символ, расположенный на ленте памяти начиная с адреса begin
+//или на первый ноль-символ.
 char *findSpace(char *begin) {
     while (*begin != '\0' && !isspace(*begin))
         begin++;
@@ -32,6 +41,10 @@ char *findSpace(char *begin) {
     return begin;
 }
 
+//№5 возвращает указатель на первый справа символ, отличный от пробельных,
+//расположенный на ленте памяти, начиная с rbegin (последний символ
+//строки, за которым следует ноль-символ) и заканчивая rend (адрес символа перед началом строки). Если символ не найден, возвращается адрес
+//rend
 char *findNonSpaceReverse(char *rbegin, const char *rend) {
     while (rbegin != rend && isspace(*rbegin))
         rbegin--;
@@ -39,13 +52,17 @@ char *findNonSpaceReverse(char *rbegin, const char *rend) {
     return rbegin;
 }
 
+//№6 возвращает указатель на первый пробельный символ справа, расположенный на ленте памяти, начиная с rbegin и заканчивая rend. Если символ не найден,
+//возвращается адрес rend
 char *findSpaceReverse(char *rbegin, const char *rend) {
     while (rbegin != rend && !isspace(*rbegin))
         rbegin--;
 
     return rbegin;
 }
-
+//№7 Функция возвращает отрицательное значение, если lhs располагается до rhs
+//в лексикографическом порядке (как в словаре), значение 0, если lhs и rhs
+//равны, иначе – положительное значение.
 int strcmp(const char *lhs, const char *rhs) {
     while (*lhs != '\0' && (*lhs == *rhs))
         lhs++, rhs++;
@@ -53,6 +70,10 @@ int strcmp(const char *lhs, const char *rhs) {
     return *lhs - *rhs;
 }
 
+//№7 записывает по адресу beginDestination
+//фрагмент памяти, начиная с адреса beginSource до endSource.
+//Возвращает указатель на следующий свободный фрагмент памяти в
+//destination
 char *copy(const char *beginSource, const char *endSource,
            char *beginDestination) {
     memcpy(beginDestination, beginSource, endSource - beginSource);
@@ -61,6 +82,12 @@ char *copy(const char *beginSource, const char *endSource,
 
     return destination;
 }
+
+//№8 записывает по адресу
+//beginDestination элементы из фрагмента памяти начиная с beginSource
+//заканчивая endSource, удовлетворяющие функции-предикату f. Функция
+//возвращает указатель на следующий свободный для записи фрагмент в
+//памяти.
 
 char *copyIf(char *beginSource, const char *endSource,
              char *beginDestination, int (*f)(int)) {
@@ -84,6 +111,10 @@ char *copyReverse(char *rbeginSource, const char *rendSource, char *beginDestina
     return beginDestination;
 }
 
+//№9 записывает по адресу
+//beginDestination элементы из фрагмента памяти начиная с rbeginSource
+//заканчивая rendSource, удовлетворяющие функции-предикату f. Функция возвращает значение beginDestination по окончанию работы
+//функции
 char *copyIfReverse(char *rbeginSource, const char *rendSource,
                     char *beginDestination, int (*f)(int)) {
     while (rbeginSource != rendSource) {
